d1c94a15f8031813c11d6e6f5864956437c56a69
Junior guinier <guinier95.js@gmail.com>
2024-10-22 10:22:50 +0200

    logic de fermeture de la modale

---
+++ modal.js
@@ -0,0 +1,31 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+//close modal form
+function closeModal() {
+  const crossClose = document.querySelector(".close");
+  crossClose.addEventListener("click", () => {
+    modalbg.style.display = "none";
+  });
+ 
+}
+
+closeModal();
